776b1c6c884c3276f37fbaf451af27655f0096fb
Guannan Wei <kiss.kraks@gmail.com>
2017-11-01 16:02:39 -0400

    Add transformations from LT/GT/LEQ/NEQ to GEQ

--- src/main/scala/Omega.scala
+++ src/main/scala/Omega.scala
@@ -66,6 +66,7 @@ object Constraint {
       case ((minv,mini), (x,i)) => if (ordering.lt(x,minv)) (x,i) else (minv, mini)
     })
   }
+
   def removeZeroCoef(coefs: List[Int], vars: List[String]): (List[Int], List[String]) = {
     val cvpairs = for ((c, v) <- (coefs zip vars) if !(c == 0 && v != const)) yield (c, v)
     // TODO: may refactor this to only use one pass
@@ -136,7 +137,6 @@ trait Constraint[C <: Constraint[C]] {
     val newVars = removeByIdx(vars, idx)
     (newCoefs, newVars)
   }
-
   
   //TODO: better rename this function
   def _subst(x: String, term: (List[Int], List[String])): (List[Int], List[String]) = {
@@ -150,7 +150,7 @@ trait Constraint[C <: Constraint[C]] {
     reorder(oldCoefs++newCoefs, oldVars++newVars)
   }
   
-  /* Finds the minimum absolute value of coefficient.
+  /* Finds the minimum absolute value of coefficient, except the constant term.
    * Returns ((value, var) index).
    */
   def minCoef(): ((Int, String), Int) = { 
@@ -379,6 +379,41 @@ case class GEQ(coefficients: List[Int], vars: List[String]) extends Constraint[G
   }
 }
 
+case class GT(coefficients: List[Int], vars: List[String]) {
+  /* Transforms \Sigma a_i x_i > 0 to \Sigma a_i x_i >= 1
+   */
+  def toGEQ: List[GEQ] = {
+    val (newCoefs, newVars) = reorder(-1::coefficients, const::vars)
+    List(GEQ(newCoefs, newVars))
+  }
+}
+
+case class LT(coefficients: List[Int], vars: List[String]) {
+  /* Transforms \Sigma a_i x_i < 0 to \Sigma -1 * a_i x_i >= 1
+   */
+  def toGEQ: List[GEQ] = {
+    val (newCoefs, newVars) = reorder(-1::scale(coefficients, -1), const::vars)
+    List(GEQ(newCoefs, newVars))
+  }
+}
+
+case class LEQ(coefficients: List[Int], vars: List[String]) {
+  /* Transforms \Sigma a_i x_i <= 0 to \Sigma -1 * a_i x_i >= 0
+   */
+  def toGEQ: List[GEQ] = {
+    List(GEQ(scale(coefficients, -1), vars))
+  }
+}
+
+case class NEQ(coefficients: List[Int], vars: List[String]) {
+  /* Transforms \Sigma a_i x_i =/= 0 to \Sigma a_i x_i >= 1 and \Sigma a_i x_i <= -1
+   */
+  def toGEQ: List[GEQ] = {
+    val (coefs1, vars1) = reorder(-1::coefficients, const::vars)
+    val (coefs2, vars2) = reorder(-1::scale(coefficients, -1), const::vars)
+    List(GEQ(coefs1, vars1), GEQ(coefs2, vars2))
+  }
+}
 object Problem {
   var varIdx = 0
   val greeks = List("α", "β", "γ", "δ", "ϵ", "ζ", "η", "θ", "ι", "κ", "λ", "μ",
@@ -795,7 +830,17 @@ object OmegaTest {
     assert(!p8ans)
     println(s"p8 has integer solutions: ${p8ans}")
     
+    val p8_1 = Problem(NEQ(List(1, 2, 2), List(const, "m", "n")).toGEQ)
+    println(s"p8_1: $p8_1")
+    val p8_1ans = p8_1.hasIntSolutions
+    assert(!p8_1ans)
+    println(s"p8_1 has integer solutions: ${p8ans}")
     println("an omega test nightmare")
+    /* 45 - 11x - 13y >= 0
+     * -27 + 11x + 13y >= 0
+     *  4 + -7x + 9y >= 0
+     *  10 + 7x - 9y >= 0
+     */
     val p9 = Problem(List(GEQ(List(45, -11, -13), List(const, "x", "y")),
                           GEQ(List(-27, 11, 13), List(const, "x", "y")),
                           GEQ(List(4, -7, 9), List(const, "x", "y")),
