c2d9706400c4ca66a18d7e7cbacdfb6ea7547d94
Gregory Essertel <gregory.essertel@gmail.com>
2017-11-11 16:19:17 -0500

    Pointer dereference

--- src/main/scala/Main.scala
+++ src/main/scala/Main.scala
@@ -11,11 +11,62 @@ import IRD._
 object MyMain {
   def main(arr: Array[String]) = {
     val code = """
+    #define NULL 0
+    struct list {
+      int value;
+      struct list* next;
+    };
     int main() {
-      int n = __VERIFIER_nondet_int();
-      if (n > 100)
-        n = 100;
-      assert(n <= 100);
+      int n = 5;
+      struct list* x = (struct list *) NULL; // malloc(sizeof(struct list));
+      int i = 0;
+      while (i < n) {
+        struct list* y = (struct list *) malloc(sizeof(struct list));
+        y->value = i;
+        y->next = x;
+        x = y;
+        i = i + 1;
+      }
+
+      struct list* z = x;
+      int agg = 0;
+      i = 0;
+      while (i < n) {
+        agg = agg + z->value;
+        z = z->next;
+        i = i + 1;
+      }
+
+      assert(2 * agg == n * (n - 1));
+      return 0;
+    }
+    """
+    val code1 = """
+    #define NULL 0
+    struct list {
+      int value;
+      struct list* next;
+    };
+    int main() {
+      int n = 5;
+      struct list* x = (struct list *) NULL; // malloc(sizeof(struct list));
+      int i = 0;
+      while (i < n) {
+        struct list* y = (struct list *) malloc(sizeof(struct list));
+        y->value = i;
+        y->next = x;
+        x = y;
+        i = i + 1;
+      }
+
+      struct list* z = x;
+      int agg = 0;
+      while (z != NULL) {
+        agg = agg + z->value;
+        z = z->next;
+      }
+
+      assert(2 * agg == n * (n - 1));
       return 0;
     }
     """
@@ -27,26 +78,61 @@ object MyMain {
     };
     int main() {
       int n = __VERIFIER_nondet_int();
-      struct list* x = NULL;
+      if (n < 0) n = 0;
+      struct list* x = (struct list *) NULL; // malloc(sizeof(struct list));
       int i = 0;
       while (i < n) {
         struct list* y = (struct list *) malloc(sizeof(struct list));
-        y->tail = x;
-        y->tail = i++;
+        y->value = i;
+        y->next = x;
         x = y;
-      }
-      struct list* z = x;
-      int s = 0;
-      while (z != NULL) {
-        s = s + z->head;
-        z = z->tail;
+        i = i + 1;
       }
 
-      assert(2*s == n*(n-1));
+      struct list* z = x;
+      int agg = 0;
+      i = 0;
+      while (i < n) {
+        agg = agg + z->value;
+        z = z->next;
+        i = i + 1;
+      }
+
+      assert(2 * agg == n * (n - 1));
       return 0;
     }
     """
-    val parsed = parseCString(code)
+    val code3 = """
+    #define NULL 0
+    struct list {
+      int value;
+      struct list* next;
+    };
+    int main() {
+      int n = __VERIFIER_nondet_int();
+      if (n < 0) n = 0;
+      struct list* x = (struct list *) NULL; // malloc(sizeof(struct list));
+      int i = 0;
+      while (i < n) {
+        struct list* y = (struct list *) malloc(sizeof(struct list));
+        y->value = i;
+        y->next = x;
+        x = y;
+        i = i + 1;
+      }
+
+      struct list* z = x;
+      int agg = 0;
+      while (z != NULL) {
+        agg = agg + z->value;
+        z = z->next;
+      }
+
+      assert(2 * agg == n * (n - 1));
+      return 0;
+    }
+    """
+    val parsed = parseCString(code2)
     val cfgs = fileToCFG(parsed)
 
     evalCfgUnit(parsed)
