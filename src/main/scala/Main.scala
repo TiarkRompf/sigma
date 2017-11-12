2ea11a4b5b0c30342f16512a47fcf316b132b188
Guannan Wei <kiss.kraks@gmail.com>
2017-11-12 05:22:54 -0500

    misc

--- src/main/scala/Main.scala
+++ src/main/scala/Main.scala
@@ -162,7 +162,7 @@ object MyMain {
       return 0;
     }
     """
-    val parsed = parseCString(code1)
+    val parsed = parseCString(code)
     val cfgs = fileToCFG(parsed)
 
     evalCfgUnit(parsed)
@@ -178,11 +178,9 @@ object MyMain {
     // Should be something like this for simple_code
     // { -100 + 1x0? >= 0 } ==> { 0 = 0 } && 
     // { 99 - 1x0? >= 0 } ==> { 100 - 1x0? >= 0 }
-    /*
     val validOmega = translate(valid.get)
     println(validOmega)
     assert(verify(validOmega))
-    */
     
   }
 }
